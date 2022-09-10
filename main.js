1becee8307fe314d8ff9a2526a42feaeec2bf891
josueventura07 <josueandresventura@gmail.com>
2022-09-10 13:13:04 -0400

    primera actualizacion

---
+++ main.js
@@ -0,0 +1,51 @@
+const iconMenu = document.querySelector('.bx-menu');
+const menu = document.querySelector('.deployMenu');
+const btnHome = document.querySelector(".btn-home");
+const btnAbout = document.querySelector(".btn-about");
+const btnSkills = document.querySelector(".btn-skills");
+const btnContact = document.querySelector(".btn-contact");
+const hiddenNav = document.querySelector('#hiddenNav')
+
+iconMenu.addEventListener('click', (e) => {
+    menu.classList.toggle('deployMenu');
+})
+
+
+    btnHome.addEventListener('click', (e) => {
+        if(e.target) {
+            menu.classList.toggle('deployMenu');   
+        }
+
+    })
+    
+    btnAbout.addEventListener('click', (e) => {
+        if(e.target) {
+            menu.classList.toggle('deployMenu');   
+        }
+
+    }) 
+
+    btnSkills.addEventListener('click', (e) => {
+        if(e.target) {
+            menu.classList.toggle('deployMenu');   
+        }
+
+    }) 
+
+    btnContact.addEventListener('click', (e) => {
+        if(e.target) {
+            menu.classList.toggle('deployMenu');   
+        }
+
+    }) 
+    
+    window.addEventListener('scroll', () => {
+        if(window.scrollY >= 50) {
+            hiddenNav.classList.add('hiddenNav')
+            
+        } else {
+            hiddenNav.classList.remove('hiddenNav')
+        }
+    })
+
+
